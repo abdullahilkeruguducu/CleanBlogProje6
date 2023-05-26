a15afcd89f2b4429e2e5527db6e9a54b882f9d5b
Abdullah Ilker UGUDUCU <abdullahilkeruguducu@gmail.com>
2023-05-26 12:54:43 +0300

    First Commit

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const express = require("express");
+const ejs = require("ejs");
+const path = require("path");
 
 const app = express();
 
+//Template engine
+app.set("view engine", "ejs");
+
+//Middleware
+app.use(express.static("public"));
+
+//Routes
+
 app.get("/", (req, res) => {
-  const blog = {
-    id: 1,
-    title: "Blog title",
-    description: "Blog description",
-  };
-  res.send(blog);
+  res.render("index");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/add", (req, res) => {
+  res.render("add_post");
 });
 
 const port = 3000;
