acdc9c69be7d98348268a7bd829b5d4b0eb96e42
Abdullah Ilker UGUDUCU <abdullahilkeruguducu@gmail.com>
2023-06-03 17:37:15 +0300

    Clean Blog 3. Proje yapildi

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const ejs = require("ejs");
 const path = require("path");
+const Post = require("./models/Post");
 
 const app = express();
+
+//connect DB
+
+mongoose.connect("mongodb://127.0.0.1:27017/cleanblog-test-db", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 //Template engine
 app.set("view engine", "ejs");
 
 //Middleware
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //Routes
 
-app.get("/", (req, res) => {
-  res.render("index");
+app.get("/", async (req, res) => {
+  const posts = await Post.find({});
+  res.render("index", {
+    posts,
+  });
 });
 
 app.get("/about", (req, res) => {
@@ -22,6 +36,12 @@ app.get("/about", (req, res) => {
 
 app.get("/add", (req, res) => {
   res.render("add_post");
+});
+
+app.post("/posts", async (req, res) => {
+  console.log(req.body);
+  Post.create(req.body);
+  res.redirect("/");
 });
 
 const port = 3000;
