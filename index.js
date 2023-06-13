e92aabf45b330dfa024eddcc2a8d9a638bc2492e
Abdullah Ilker UGUDUCU <abdullahilkeruguducu@gmail.com>
2023-06-13 22:32:28 +0300

    Update ve delete eklendi MVC Duzenlendi.

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 const ejs = require("ejs");
 const path = require("path");
 const Post = require("./models/Post");
-
+const fs = require("fs");
 const app = express();
+const postControllers = require("./controllers/postControllers");
+const pageControllers = require("./controllers/pageControllers");
 
 //connect DB
 
@@ -20,36 +23,22 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(
+  methodOverride("_method", {
+    methods: ["POST", "GET"],
+  })
+);
 
 //Routes
 
-app.get("/", async (req, res) => {
-  const posts = await Post.find({});
-  res.render("index", {
-    posts,
-  });
-});
-
-app.get("/posts/:id", async (req, res) => {
-  const post = await Post.findById(req.params.id);
-  res.render("post", {
-    post,
-  });
-});
-
-app.get("/about", (req, res) => {
-  res.render("about");
-});
-
-app.get("/add", (req, res) => {
-  res.render("add_post");
-});
-
-app.post("/posts", async (req, res) => {
-  console.log(req.body);
-  Post.create(req.body);
-  res.redirect("/");
-});
+app.get("/", postControllers.getAllPosts);
+app.get("/posts/:id", postControllers.getPost);
+app.get("/about", pageControllers.getAboutPage);
+app.get("/add", pageControllers.getAddPage);
+app.post("/posts", postControllers.createPost);
+app.get("/posts/edit/:id", pageControllers.getEditPage);
+app.put("/posts/:id", postControllers.updatePost);
+app.delete("/posts/:id", postControllers.deletePost);
 
 const port = 3000;
 app.listen(port, () => {
--- models/Post.js
+++ models/Post.js
@@ -5,9 +5,10 @@ const Schema = mongoose.Schema;
 const PostSchema = new Schema({
   title: String,
   detail: String,
+  author: String,
   dateCreated: {
     type: Date,
-    default: Date.now,
+    default: Date.now(),
   },
 });
 
